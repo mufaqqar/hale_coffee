45e74806ac628e31b4be70ab0336b67d3bb0c902
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2026-08-18 16:11:27 +0500

    heade menu

--- header.php
+++ header.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
+
 <body <?php body_class('antialiased'); ?>>
     <?php wp_body_open(); ?>
     <div id="page" class="min-h-screen flex flex-col">
@@ -16,10 +18,11 @@
                         <a href="<?php echo esc_url(home_url('/')); ?>" class="inline-flex">
                             <?php the_custom_logo(); ?>
                         </a>
-                    
+
                     <?php else: ?>
                         <a href="<?php echo esc_url(home_url('/')); ?>" class="inline-flex">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="Logo" height="60" width="60" />
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="Logo"
+                                height="60" width="60" />
                         </a>
                     <?php endif; ?>
                 </div>
@@ -31,7 +34,7 @@
                     <?php
                     wp_nav_menu([
                         'theme_location' => 'primary',
-                        'menu_class' => 'flex justify-center space-x-6 list-none m-0 p-0',
+                        'menu_class' => 'flex justify-center md:space-x-6 list-none m-0 md:p-0 p-3 md:flex-row flex-col',
                         'container' => false,
                         'fallback_cb' => false,
                         'walker' => new Hale_Mega_Walker(),
